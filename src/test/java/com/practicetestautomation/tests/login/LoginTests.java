1a2fbcf1898930c27da41fe4e1ee6bd33cd2e65d
Vitali Dolgoshei <vitali_dauhashei@senla.eu>
2025-01-13 16:31:07 +0300

    Before parametrizing TestNG Negative methods

--- src/test/java/com/practicetestautomation/tests/login/LoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/LoginTests.java
@@ -6,16 +6,41 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class LoginTests {
+    private WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("browser")
+    public void setup(@Optional("chrome") String browser) {
+        System.out.println("Runnig test in " + browser);
+
+        switch (browser.toLowerCase()) {
+            case "chrome" :
+                driver = new ChromeDriver();
+                break;
+            case "firefox" :
+                driver = new FirefoxDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
+                break;
+        }
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+
     @Test(groups={"positive", "regression", "smoke"})
     public void testLoginFunctionality(){
         // Open page
         //WebDriver driver = new ChromeDriver();
-        WebDriver driver = new FirefoxDriver();
-
-        driver.get("https://practicetestautomation.com/practice-test-login/");
+        //WebDriver driver = new FirefoxDriver();
+        //driver.get("https://practicetestautomation.com/practice-test-login/");
 
         // Type username student into Username field
         WebElement usernameField = driver.findElement(By.id("username"));
@@ -51,25 +76,26 @@ public class LoginTests {
         //WebElement logOutButton = driver.findElement(By.linkText("Log in"));
         Assert.assertTrue(logOutButton.isDisplayed(), "Button LogOut was found");
 
-        driver.quit();
+        //driver.quit();
 
     }
 
+    @Parameters({"username", "password", "expectedErrorMessage"})
     @Test(groups={"negative", "regression"})
-    public void incorrectUserNameTest() {
-        WebDriver driver = new ChromeDriver();
-        System.setProperty("webdriver.edge.driver", "src/main/resources/msedgedriver.exe");
+    public void negativeLoginTest(String username, String password, String expectedErrorMessage) {
+        //WebDriver driver = new ChromeDriver();
+        //System.setProperty("webdriver.edge.driver", "src/main/resources/msedgedriver.exe");
         //WebDriver driver = new EdgeDriver();
+        //driver.get("https://practicetestautomation.com/practice-test-login/");
 
-        driver.get("https://practicetestautomation.com/practice-test-login/");
 
         // Type username wrong_student into Username field
         WebElement usernameField = driver.findElement(By.id("username"));
-        usernameField.sendKeys("Wrong_student");
+        usernameField.sendKeys(username);
 
         // Type password Password123 into Password field
         WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.sendKeys("Password123");
+        passwordField.sendKeys(password);
 
         // Push Submit button
         WebElement submitButton = driver.findElement(By.id("submit"));
@@ -81,58 +107,19 @@ public class LoginTests {
             throw new RuntimeException(e);
         }
 
-        // Verify Error message is displayed
+        // Verify Error message is displayed    Display message: Your username is invalid!
         WebElement usernameInvalidMessage = null;
         try {
             usernameInvalidMessage = driver.findElement(By.id("error"));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        Assert.assertTrue(usernameInvalidMessage.isDisplayed(), "Display message: Your username is invalid!");
+
+        //Assert.assertTrue(usernameInvalidMessage.isDisplayed(), expectedErrorMessage);
 
         // Verify Error message text is Your username is invalid!
-        Assert.assertEquals(usernameInvalidMessage.getText(), "Your username is invalid!", "Message content");
+        Assert.assertEquals(usernameInvalidMessage.getText(), expectedErrorMessage, "Message content");
 
-        driver.quit();
-
+        //driver.quit();
     }
-
-    @Test(groups={"negative", "regression"})
-    public void incorrectPasswordTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://practicetestautomation.com/practice-test-login/");
-
-        // Type username student into Username field
-        WebElement usernameField = driver.findElement(By.id("username"));
-        usernameField.sendKeys("student");
-
-        // Type password Password123 into Password field
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.sendKeys("Weong_Password123");
-
-        // Push Submit button
-        WebElement submitButton = driver.findElement(By.id("submit"));
-        submitButton.click();
-
-        try {
-            Thread.sleep(2000); // pause 2 sec
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Verify Error message is displayed
-        WebElement usernameInvalidMessage = null;
-        try {
-            usernameInvalidMessage = driver.findElement(By.id("error"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        Assert.assertTrue(usernameInvalidMessage.isDisplayed(), "Display message: Your password is invalid!");
-
-        // Verify Error message text is Your username is invalid!
-        Assert.assertEquals(usernameInvalidMessage.getText(), "Your password is invalid!", "Message content");
-
-        driver.quit();
-    }
-
 }
