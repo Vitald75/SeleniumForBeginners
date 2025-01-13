1d5594f26d0d7241c0341f17c4a346c5f082ee58
Vitali Dolgoshei <vitali_dauhashei@senla.eu>
2025-01-13 14:27:50 +0300

    Before parametrizing TestNG Negative methods

--- src/test/java/com/practicetestautomation/tests/login/LoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/LoginTests.java
@@ -4,14 +4,63 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NegativeLoginTests {
+public class LoginTests {
+    @Test(groups={"positive", "regression", "smoke"})
+    public void testLoginFunctionality(){
+        // Open page
+        //WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
 
-    @Test
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        // Type username student into Username field
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys("student");
+        //usernameField.sendKeys("wrong_student");
+
+        // Type password Password123 into Password field
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("Password123");
+
+        // Push Submit button
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+        try {
+            Thread.sleep(2000); // pause 2 sec
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+        String expectedUrl = "https://practicetestautomation.com/logged-in-successfully/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "url matches");
+
+        //Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        String expectedMessage = "Congratulations student. You successfully logged in!";
+        String pageSource = driver.getPageSource();
+        Assert.assertTrue(pageSource.contains(expectedMessage), "Message was found");
+
+        // Verify button Log out is displayed on the new page
+        WebElement logOutButton = driver.findElement(By.linkText("Log out"));
+        //WebElement logOutButton = driver.findElement(By.linkText("Log in"));
+        Assert.assertTrue(logOutButton.isDisplayed(), "Button LogOut was found");
+
+        driver.quit();
+
+    }
+
+    @Test(groups={"negative", "regression"})
     public void incorrectUserNameTest() {
         WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.edge.driver", "src/main/resources/msedgedriver.exe");
+        //WebDriver driver = new EdgeDriver();
+
         driver.get("https://practicetestautomation.com/practice-test-login/");
 
         // Type username wrong_student into Username field
@@ -48,7 +97,7 @@ public class NegativeLoginTests {
 
     }
 
-    @Test
+    @Test(groups={"negative", "regression"})
     public void incorrectPasswordTest() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
@@ -85,4 +134,5 @@ public class NegativeLoginTests {
 
         driver.quit();
     }
+
 }
